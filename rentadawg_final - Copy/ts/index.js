6d7be0db6c9b15cb726513a2ba26cd364d0ad05e
Lea :) <leahvgoretoy@gmail.com>
2025-03-18 12:01:25 -0600

    Add files via upload

---
+++ rentadawg_final - Copy/ts/index.js
@@ -0,0 +1,16 @@
+"use strict";
+// 2/24/25 added code to make sure prompt only pops up once per user session
+if (!sessionStorage.getItem("dogBreed")) {
+    const dogBreed_1 = (prompt("Quick question! What's your favorite dog breed? "));
+    if (dogBreed_1) {
+        sessionStorage.setItem("dogBreed", dogBreed_1);
+    }
+}
+const dogBreed = sessionStorage.getItem("dogBreed") || "all type";
+// 2/24/25 removed for simplicity ---> alert (dogBreed +"! Gotcha!!");
+const listElement = document.getElementById("list");
+if (listElement) {
+    listElement.innerText = "Have you felt like your parties are a little lack-luster lately?" +
+        " Well, bow-how about a party rockin dj at your next event! From choosing the dog breed (we heard ya like "
+        + dogBreed + "(s)), to the party style, to music type, and so much more. Rent-A-Dawg has all the answers to your doggie dj dreams!";
+}
